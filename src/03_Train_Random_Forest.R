79b13ffd573848589bb3d6bbc520191c4c40004e
Kyle Taylor <kyle.a.taylor@gmail.com>
2016-03-14 09:28:42 -0600

    house keeping

--- src/03_Train_Random_Forest.R
+++ src/03_Train_Random_Forest.R
@@ -3,7 +3,8 @@ require(rgdal)
 require(randomForest)
 require(rfUtilities)
 
-argv <- commandArgs(trailingOnly=T)
+#argv <- commandArgs(trailingOnly=T)
+argv <- "focal_county_146"
 
 #
 # Local functions
@@ -43,10 +44,10 @@ qaCheck_checkBalancedClasses<- function(t,correct=F){
 # qaCheck_multiColinearity()
 #
 qaCheck_multiColinearity <- function(t){
-  coVars <- suppressWarnings(rfUtilities::multi.collinear(training_table))
+  coVars <- suppressWarnings(rfUtilities::multi.collinear(t))
   if(length(coVars)>0){
     cat(" -- (warning) dropped variables due to multicolinearity:",coVars,"\n",sep="")
-    t <- t[,grepl(names(t),pattern=paste(coVars,collapse="|"))]
+    t <- t[,!grepl(names(t),pattern=paste(coVars,collapse="|"))]
   }
   return(t)
 }
@@ -57,10 +58,41 @@ qaCheck_dropVarsWithPoorExplanatoryPower <- function(m=NULL,t=NULL,p=0.35,plot=F
   o <- rfUtilities::rf.imp.freq(m,p=p,plot=plot)
     o$frequency$var.freq <- o$frequency$var.freq/3
   if(sum(o$frequency$var.freq == 0)>0){
-    cat(" -- (warning) dropping uninformative variables :",as.vector(o$frequency$vars)[o$frequency$var.freq == 0],"\n",sep="")
-    t <- t[,grepl(names(t),pattern=paste(as.vector(o$frequency$vars)[o$frequency$var.freq > 0.3],collapse="|"))]
+    cat(" -- (warning) dropping uninformative variables :",paste(as.vector(o$frequency$vars)[o$frequency$var.freq == 0],collapse=" "),"\n",sep="")
+    t <- cbind(response=t[,'response'],
+               t[,grepl(names(t),pattern=paste(as.vector(o$frequency$vars)[o$frequency$var.freq > 0.3],collapse="|"))])
   }
   return(t)
+}
+#
+# chunk()
+#
+chunk <- function(x,size=200){
+  split(x, ceiling(seq_along(x)/size))
+}
+#
+# qaCheck_findConvergence()
+# Use a quick likelihood ratio to find chunks that are significantly (p>=95%) different than the tail of
+# OOB error observed in a random forest object.  Report the right-hand side (upper-bound) of the last significant
+# chunk back to the user to help establish a cut-off for re-training a forest.  This is an approximation of a moving
+# window analysis to find a value appropriate for twice-the-rate-of-convergence rule usually applied to picking an
+# appropriate ntrees parameter for randomForest.
+#
+qaCheck_findConvergence <- function(m=NULL,size=100){
+  err <- abs(diff(diff(m$err.rate[,1])))
+    err <- chunk(err,size=size)
+
+  tailTrainingData <- as.vector(unlist(err[length(err)-3:length(err)]))
+    tailTrainingData <- c(mean(tailTrainingData),sd(tailTrainingData))
+
+  means <- as.vector(unlist(lapply(err,FUN=mean)))
+    sds <- as.vector(unlist(lapply(err,FUN=sd)))
+
+  sigs <- dnorm(x=tailTrainingData[1],mean=tailTrainingData[1],sd=tailTrainingData[2],log=F)/dnorm(x=means[1:length(err)],mean=tailTrainingData[1],sd=tailTrainingData[2],log=F)
+    sigs <- sigs >= 1.96
+      sigs <- max(which(sigs==TRUE))
+
+  return(sigs*size)
 }
 #
 # MAIN
@@ -69,30 +101,32 @@ qaCheck_dropVarsWithPoorExplanatoryPower <- function(m=NULL,t=NULL,p=0.35,plot=F
 # read-in our training data
 training_pts <- readOGR(".",paste(argv,"_farmed_binary_pts",sep=""),verbose=F)
 # read-in our explanatory data
-expl_vars <- list.files(pattern=paste(argv[1],"*.*tif$",sep=""))
+expl_vars <- list.files(pattern=paste("^",argv[1],".*.tif$",sep=""))
   expl_vars <- expl_vars[!grepl(expl_vars,pattern="farmed")]
     expl_vars <- lapply(expl_vars,FUN=raster)
-      expl_vars <- try(raster::stack(expl_vars))
+      expl_vars <- raster::stack(expl_vars)
 names <- names(expl_vars)
-  names <- names[!grepl(names,pattern="county|focal")]
-    names <- names[suppressWarnings(is.na(as.numeric(names)))]
-      names <- names[which(grepl(names(expl_vars),pattern=paste(names,collapse="|")))]
-        names(expl_vars) <- names
+  names <- unlist(strsplit(names,split="_"))
+    names <- names[!grepl(names,pattern="county|focal")]
+      names <- names[suppressWarnings(is.na(as.numeric(names)))]
+        names <- names[which(grepl(names(expl_vars),pattern=paste(names,collapse="|")))]
+          names(expl_vars) <- names
 # extract across our training points
 training_pts <- training_pts[!is.na(extract(expl_vars$iccdcdpct,training_pts)),] # the geometry of our SSURGO data can be limiting here...
-training_table <- extract(expl_vars,training_pts,df=T)
-  training_table <- cbind(data.frame(response=training_pts$response),training_table[,!grepl(names(training_table),pattern="ID$")])
-
+  training_table <- extract(expl_vars,training_pts,df=T)
+    training_table <- cbind(data.frame(response=training_pts$response),training_table[,!grepl(names(training_table),pattern="ID$")])
 # QA Check our training data
 training_table$slope[is.na(training_table$slope)] <- 0
 
 training_table <- qaCheck_dropVarsWithAbundantNAs(training_table)
 training_table <- qaCheck_checkBalancedClasses(training_table)
 training_table <- qaCheck_multiColinearity(training_table)
-
 # train a preliminary forest
 m <- randomForest(as.factor(response)~.,data=training_table,importance=T,ntree=3000,do.trace=T)
 # Post-hoc QA check variable importance
 training_table <- qaCheck_dropVarsWithPoorExplanatoryPower(m,t=training_table)
-
-#
+# Re-train a final forest, optimizing based on convergence of OOB error in the final forest
+m <- randomForest(as.factor(response)~.,data=training_table,importance=T,ntree=3000,do.trace=T)
+  m <- randomForest(as.factor(response)~.,data=training_table,importance=T,ntree=qaCheck_findConvergence(mod,size=5)*2,do.trace=T)
+# Save our model object to disk
+save.image(paste(argv[1],"_model.rdata",sep=""))
