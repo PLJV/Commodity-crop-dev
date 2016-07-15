5be442ceff83a156137aa15bd37769d14df6835f
Kyle Taylor <ktaylora@users.noreply.github.com>
2016-07-14 18:40:34 -0600

    Update 02_Prepare_explanatory_data.R
    Commit from work on 2.0.

--- src/02_Prepare_explanatory_data.R
+++ src/02_Prepare_explanatory_data.R
@@ -6,6 +6,9 @@
 # design principles for v.2.0.  Adding the ability to generate training data across broader geographic extents than
 # a single county, per v.1.0.
 #
+# Accepts a single argument containing the full path to a shapefile that defines the project extent from which we will attempt
+# to fetch soil data from SSURGO.
+#
 
 include <- function(x,from="cran",repo=NULL){
   if(from == "cran"){
@@ -170,6 +173,17 @@ splitExtent <- function(e=NULL,multiple=2){
   return(extents)
 }
 #
+# parseMuaggattTable()
+#
+parseMuaggattTable <- function(x,muaggatt.vars=NULL){
+  in.statement <- format_SQL_in_statement(as.character(x@data$mukey))  # generate a SQL statement from MUKEYs
+  q <- paste("SELECT muaggatt.mukey,", paste(muaggatt.vars,collapse=", "),
+             " FROM muaggatt JOIN component ON muaggatt.mukey = component.mukey",
+             " WHERE muaggatt.mukey IN ", in.statement,
+             "ORDER BY muaggatt.mukey ASC", sep="")
+  return(q)
+}
+#
 # extentToSoilDBCoords()
 #
 extentToSoilDBCoords <- function(e,useFloorCeiling=F) {
@@ -222,15 +236,22 @@ calcMultiplier_mapunitGeomFile <- function(x){
 #
 # extentToSsurgoSpatialPolygons()
 #
-extentToSsurgoSpatialPolygons <- function(x){
+extentToSsurgoSpatialPolygons <- function(x, multiplier=NULL){
   sizeHeurstics = data.frame(area=NA,multiplier=NA)
-     multiplier = 1.015
+  
   # NRCS gateway sees input in NAD83 by default -- also, it will not return an intersect.  Rather, it takes all intersecting features completely within a bounding box.
   # Buffer the current extent to allow for some wiggle-room.  We will walk down our extent as needed, through heuristic optimization.
-  e <- extentToSoilDBCoords(multiplyExtent(extent(spTransform(x,CRS(projection("+init=epsg:4269")))),extentMultiplier=multiplier)) # NRCS gateway sees input in NAD83 by default
+  if(is.null(multiplier)){
+    e <- extentToSoilDBCoords(extent(spTransform(x,CRS(projection("+init=epsg:4269")))))
+  } else {
+    e <- extentToSoilDBCoords(multiplyExtent(extent(spTransform(x,CRS(projection("+init=epsg:4269")))),extentMultiplier=multiplier)) # NRCS gateway sees input in NAD83 by default
+  
     sizeHeurstics[1,1] <- diff(c(e[1],e[3]))*diff(c(e[2],e[4])) # area of our bounding box in degrees^2
     sizeHeurstics[1,2] <- multiplier                            # multiplier constant applied for our initial step
+  }
+  
   e <- try(mapunit_geom_by_ll_bbox(e))
+  
   if(class(e) == "try-error"){
     while(class(e) == "try-error" && nrow(sizeHeurstics)<20){ # if we can't do this in 20 steps, there's a real problem...
       cat("\n -- sleeping for 10 seconds to hobble our server requests\n")
@@ -242,24 +263,57 @@ extentToSsurgoSpatialPolygons <- function(x){
       e <- try(mapunit_geom_by_ll_bbox(e));
     }
   }
+  
   if(class(e) == "try-error") stop("repeated failure trying to download units in BBOX for focal area")
 
   projection(e) <- CRS(projection("+init=epsg:4269"))
     return(e)
 }
 #
-# parseMuaggattTable()
+# fetchSsurgoData()
 #
-parseMuaggattTable <- function(x,muaggatt.vars=NULL){
-  in.statement <- format_SQL_in_statement(as.character(x@data$mukey))  # generate a SQL statement from MUKEYs
-  q <- paste("SELECT muaggatt.mukey,", paste(muaggatt.vars,collapse=", "),
-             " FROM muaggatt JOIN component ON muaggatt.mukey = component.mukey",
-             " WHERE muaggatt.mukey IN ", in.statement,
-             "ORDER BY muaggatt.mukey ASC", sep="")
-  return(q)
+fetchSsurgoData <- function(x,nCores=NULL){
+      # set-up a cluster for parallelization
+      if(is.null(nCores)){
+        parallel::detectCores()-2
+      }
+      cl <- makeCluster(nCores)
+      # fetch and rasterize some SSURGO data
+      focal_polygons <- extentToSsurgoSpatialPolygons(x)
+      # now get component and horizon-level data for these map unit keys
+      res <- unique(SDA_query(parseMuaggattTable(focal_polygons,muaggatt.vars=muaggatt_variables)))
+      # reclassify some of our categorical information into ordinal classes
+  
+      # merge to our shapefile
+      focal_polygons@data <- merge(focal_polygons@data,res,by="mukey")
+  
+      # convert our categorical variables to ordinal variables that are tractable for RandomForests
+      focal_polygons$flodfreqmax <- floodFrequencyToOrdinal(focal_polygons$flodfreqmax)
+      focal_polygons$drclassdcd  <- drainageToOrdinal(focal_polygons$drclassdcd)
+      focal_polygons$flodfreqdcd <- floodFrequencyToOrdinal(focal_polygons$flodfreqdcd)
+      focal_polygons$hydgrpdcd   <- hydgrpToOrdinal(focal_polygons$hydgrpdcd)
+  
+      # convert to rasters
+      cat(" -- cropping SSURGO vectors to the extent of the focal county\n")
+      focal_polygons <- spTransform(focal_polygons,CRS(projection(x)))
+        focal_polygons <- focal_polygons[names(focal_polygons)[!grepl(names(focal_polygons),pattern="mu|vers|area")]] # drop unnecessary variables
+            #focal_polygons <- raster::crop(focal_polygons,s)
+      cat(" -- generating gridded raster surfaces from SSURGO polygons\n")
+      out <- list(); # today, my brain can't make splitting a SpatialPolygons file by field into a list happen for some reason
+        for(i in 1:length(names(focal_polygons))){ out[[length(out)+1]] <- focal_polygons[,names(focal_polygons)[i]] }
+          f <- function(x,y=NULL,progress=NULL){ require(raster); return(rasterize(x[,1],raster(extent(x),res=30),update=T,field=names(x[,1]),progress=progress)) }
+            out <- parLapply(cl,out,fun=f,progress=NULL)
+              out <- raster::stack(out)
+                names(out) <- names(focal_polygons)
+                
+      # clean-up
+      endCluster();
+      
+      # return list to user
+      return(out)
 }
 #
-# unpack()
+# unpackClimateVars()
 #
 unpackClimateVars <- function(x){
   filename <- substr(x,1,nchar(x)-4) # i.e., filename root; minus extension
@@ -323,7 +377,10 @@ fetchTopographicData <- function(x,useLocal=FALSE){
 snapTo <- function(x,to=NULL,names=NULL,method='bilinear'){
   require(parallel)
   # set-up a cluster for parallelization
-  cl <- makeCluster((parallel::detectCores()-4))
+  if(is.null(nCores)){
+    nCores <- parallel::detectCores()-2
+  }
+  cl <- makeCluster(nCores)
   # crop, reproject, and snap our raster to a resolution and projection consistent with the rest our explanatory data
   if(grepl(tolower(class(x)),pattern="character")){ lapply(x,FUN=raster) }
   e <- as(extent(to[[1]]),'SpatialPolygons')
@@ -361,56 +418,69 @@ lWriteRaster <- function(x,y,cName=NULL){
     raster::writeRaster(x,filename=paste(cName,"_",y,".tif",sep=""),overwrite=T,format="GTiff")
   }
 }
+#
+# parseLayerDsn()
+# 
+parseLayerDsn <- function(x=NULL){
+  path <- unlist(strsplit(x, split="/"))
+    layer <- gsub(path[length(path)],pattern=".shp",replacement="")
+      dsn <- paste(path[1:(length(path)-1)],collapse="/")
+  return(c(layer,dsn))
+}
+#
+# readOGRfromPath()
+# 
+readOGRfromPath <- function(path=NULL){
+  include('rgdal')
+  path <- parseLayerDsn(path)
+   
+  layer <- path[1]
+    dsn <- path[2]
+
+  return(readOGR(dsn,layer,verbose=F))
+}
 
 #
 # MAIN
 #
+
 main <- function(){
+  dir.create("/home/ktaylora/tmp")
+  tempfile(tmpdir="/home/ktaylora/tmp")
   system("clear"); cat("## Commodity Crop Production Suitability Model (v.2.0) ##\n\n")
 
   # accept input data from the user demonstrating the extent of our study area
-  s <- readOGR(".",argv[1],verbose=F)
-  # calculate our SSURGO soil variables, if needed
-  if(sum(grepl(list.files(pattern=paste(argv[1],".*.tif$",sep="")),pattern=paste(muaggatt_variables,collapse='|'))) < length(muaggatt_variables)){
-    # set-up a cluster for parallelization
-    cl <- makeCluster(parallel::detectCores()-4)
-    # fetch and rasterize some SSURGO data
-    county_polygons <- extentToSsurgoSpatialPolygons(s)
-    # now get component and horizon-level data for these map unit keys
-    res <- unique(SDA_query(parseMuaggattTable(county_polygons,muaggatt.vars=muaggatt_variables)))
-    # reclassify some of our categorical information into ordinal classes
-
-    # merge to our shapefile
-    county_polygons@data <- merge(county_polygons@data,res,by="mukey")
-
-    # convert our categorical variables to ordinal variables that are tractable for RandomForests
-    county_polygons$flodfreqmax <- floodFrequencyToOrdinal(county_polygons$flodfreqmax)
-    county_polygons$drclassdcd  <- drainageToOrdinal(county_polygons$drclassdcd)
-    county_polygons$flodfreqdcd <- floodFrequencyToOrdinal(county_polygons$flodfreqdcd)
-    county_polygons$hydgrpdcd   <- hydgrpToOrdinal(county_polygons$hydgrpdcd)
-
-    # convert to rasters
-    cat(" -- cropping SSURGO vectors to the extent of the focal county\n")
-    county_polygons <- spTransform(county_polygons,CRS(projection(s)))
-      county_polygons <- county_polygons[names(county_polygons)[!grepl(names(county_polygons),pattern="mu|vers|area")]] # drop unnecessary variables
-          county_polygons <- raster::crop(county_polygons,s)
-    cat(" -- generating gridded raster surfaces from SSURGO polygons\n")
-    out <- list(); # today, my brain can't make splitting a SpatialPolygons file by field into a list happen for some reason
-      for(i in 1:length(names(county_polygons))){ out[[length(out)+1]] <- county_polygons[,names(county_polygons)[i]] }
-        f <- function(x,y=NULL,progress=NULL){ require(raster); return(rasterize(x[,1],raster(extent(x),res=30),update=T,field=names(x[,1]),progress=progress)) }
-          out <- parLapply(cl,out,fun=f,progress=NULL)
-            lWriteRaster(out,y=names(county_polygons),cName=argv[1])
-
-    ssurgo_variables <- out
-    # clean-up
-    endCluster();
-    rm(out,f);
+  s <- readOGRfromPath(argv[1])
+  
+  # calculate extent(s) for SSURGO fetches
+  e <- extent(s)
+    e <- as(e,'SpatialPolygons')
+      projection(e) <- projection(s)
+      
+  # find an extent split that satisfies our project area extent
+  splits <- ceiling(gArea(spTransform(e,CRS(projection("+init=epsg:2163"))))/112969804) # using an arbitrary bounding box size from soildb that is known to work
+    splits <- ceiling(sqrt(splits))
+  
+  e <- splitExtent(extent(e),splits)
+  
+  # calculate our SSURGO soil variables, as needed
+  if(sum(grepl(list.files(pattern=paste(parseLayerDsn(argv[1])[1],".*.tif$",sep="")),pattern=paste(muaggatt_variables,collapse='|'))) < length(muaggatt_variables)){
+    for(i in 1:length(e)){
+      focal <- as(e[[i]],'SpatialPolygons')
+        projection(focal) <- CRS(projection(s))
+      e[[i]] <- fetchSsurgoData(focal)
+    }
+    # merge and write list of rasters to disk
+    out <- do.call(raster::merge, e)
+      out <- raster::unstack(out)
+        lWriteRaster(out,y=muaggatt_variables,cName=parseLayerDsn(argv[1])[1])
   } else {
     cat(paste(" -- existing SSURGO rasters found for ",argv[1],"; skipping generation and loading existing...\n",sep=""))
     out <- list.files(pattern=paste("^",argv[1],".*.tif$",sep=""))
       ssurgo_variables <- out[grepl(out,pattern=paste(muaggatt_variables,collapse="|"))]
         ssurgo_variables <- lapply(ssurgo_variables,FUN=raster)
   }
+  
   # prepare our aquifer data
   if(!file.exists(paste(argv[1],"_satThick_10_14.tif",sep=""))){
     cat(" -- cropping, resampling, and snapping aquifer saturated thickness so that it is consistent with our SSURGO variables\n")
@@ -425,6 +495,7 @@ main <- function(){
     cat(" -- existing saturated thickness raster found for",argv[1],"; skipping generation and loading existing...\n")
     aquiferSatThickness <- raster(paste(argv[1],"_satThick_10_14.tif",sep=""))
   }
+  
   # prepare our climate data
   if(sum(grepl(list.files(pattern=paste(argv[1],".*.tif$",sep="")),pattern=paste(climate_variables,collapse='|'))) < length(climate_variables)){
     # set-up a cluster for parallelization
